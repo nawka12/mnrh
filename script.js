291ce6a68445313d05594710c212a86de42d0620
nawka12 <kayfa.haluk.y@gmail.com>
2025-03-13 04:59:00 +0700

    verbose = false

--- script.js
+++ script.js
@@ -332,7 +332,7 @@ function updateLayoutForViewport() {
 }
 
 // Add this near the top with other constants
-const VERBOSE = true;
+const VERBOSE = false;
 
 // Add this debug logger function
 const debugLog = (...args) => {
